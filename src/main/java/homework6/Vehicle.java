7131310b3802f534f645f456c065fba0840f9272
Mariami Zedginidze <mzedginidze@credo.ge>
2026-06-06 16:42:53 +0400

    [მარიამი ზ.] HW6 - Vehicle

---
+++ src/main/java/homework6/Vehicle.java
@@ -0,0 +1,20 @@
+/**
+ * @author მარიამი ზედგინიძე
+ */
+
+package homework6;
+
+import homework4.PrimeCheck;
+
+public class Vehicle {
+    private String brand;
+    private int year;
+
+    public Vehicle(String b, int y) {
+        this.brand = b;
+        this.year = y;
+    }
+
+    public void info() {
+    }
+}
