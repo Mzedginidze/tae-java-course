dd593805f1ebedbdfe52d7094cbf5a36c7c60040
Mariami Zedginidze <mzedginidze@credo.ge>
2026-06-06 16:15:47 +0400

    [მარიამი ზ.] HW6 - Animal

---
+++ src/main/java/homework6/Animal.java
@@ -0,0 +1,18 @@
+/**
+ * @author მარიამი ზედგინიძე
+ */
+
+package homework6;
+
+public class Animal {
+    protected String name;
+
+    public Animal(String n) {
+        this.name = n;
+    }
+
+    public void makeSound() {
+        System.out.println("some sound");
+    }
+
+}
