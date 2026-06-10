f76e788daf302fdc1364e9c64a8d6862ee35d335
Mariami Zedginidze <mzedginidze@credo.ge>
2026-06-10 22:26:21 +0400

    [მარიამი ზ.] HW7 - Rectangle

---
+++ src/main/java/homework7/Rectangle.java
@@ -0,0 +1,31 @@
+/**
+ * @author მარიამი ზედგინიძე
+ */
+
+package homework7;
+
+public class Rectangle implements Shape {
+    private double width;
+    private double height;
+
+    public Rectangle(double w, double h) {
+        if (w <= 0 || h <= 0) {
+            throw new IllegalArgumentException("parameters cannot be negative");
+        }
+        this.width = w;
+        this.height = h;
+    }
+
+
+    @Override
+    public double area() {
+        return this.width * this.height;
+    }
+
+    public static void main(String[] args) {
+        Shape s1 = new Rectangle(4, 5);
+        System.out.println(s1.area()); // 20.0
+        Shape s2 = new Circle(3);
+        System.out.println(s2.area()); // 28.27
+    }
+}
