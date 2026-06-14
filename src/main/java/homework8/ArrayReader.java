84c3fb7b90f89f18beb32851d61bd0c96d41448d
Mariami Zedginidze <mzedginidze@credo.ge>
2026-06-14 23:47:10 +0400

    [მარიამი ზ.] HW8 - ArrayReader

---
+++ src/main/java/homework8/ArrayReader.java
@@ -0,0 +1,23 @@
+/**
+ * @author მარიამი ზედგინიძე
+ */
+
+package homework8;
+
+public class ArrayReader {
+    public static int readSafe(int[] arr, int index) {
+        try {
+            return arr[index];
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("ინდექსი არასწორია: " + index);
+            return -1;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {10, 20, 30};
+        System.out.println(readSafe(nums, 1));
+        System.out.println("-----------------------");
+        System.out.println(readSafe(nums, 10));
+    }
+}
