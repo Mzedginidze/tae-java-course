28c6b4a4dcbd5ca2c6f859d32a5865e4dbdd4c81
Mariami Zedginidze <mzedginidze@credo.ge>
2026-06-14 23:49:31 +0400

    [მარიამი ზ.] HW8 - InvalidAgeException

---
+++ src/main/java/homework8/InvalidAgeException.java
@@ -0,0 +1,11 @@
+/**
+ * @author მარიამი ზედგინიძე
+ */
+
+package homework8;
+
+public class InvalidAgeException extends Exception{
+    public InvalidAgeException(String message){
+        super(message);
+    }
+}
